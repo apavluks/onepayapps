313b3a438e994665a120d36b441301f77b6ec7b7
Aivars Pavluks <apavluks@yahoo.com>
2014-12-20 18:20:35 +0000

    built out navigation and added welcome controller and set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/help'
+  get 'home/loan_applications'
+
   resources :loan_applications
   
-  root to: 'loan_applications#index'
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
